bf3da4a8eb697b61664292519486b3afcf0de0b4
Ronaldo Bahia <bahiamartins@hotmail.com>
2012-04-27 13:17:22 -0300

    git for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ronaldopage::Application.config.secret_token = '693daf5b1208e443f604ff94c3ed29bcca26929fa944c8e11933ace1882eb68bc70d02c573ef6c83ea4d13c30d6fc88eef6926441bea92c9dde6003e868375dc'
